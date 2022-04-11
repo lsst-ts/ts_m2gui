6eb39020dd520950961b7c19e5145ddb67552539
Te-Wei Tsai <twtsaia@gmail.com>
2022-04-11 15:47:42 -0400

    Add layout_default.py.

---
+++ python/lsst/ts/m2gui/layout/layout_default.py
@@ -0,0 +1,73 @@
+# This file is part of ts_m2gui.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = ["LayoutDefault"]
+
+from PySide2.QtCore import Slot
+
+
+class LayoutDefault(object):
+    """Layout of the default panel.
+
+    Parameters
+    ----------
+    model : `Model`
+        Model class.
+    signal_control : `SignalControl`
+        Signal to know the control is updated or not.
+
+    Attributes
+    ----------
+    model : `Model`
+        Model class.
+    layout : `PySide2.QtWidgets.QVBoxLayout`
+        Layout.
+    """
+
+    def __init__(self, model, signal_control):
+
+        self.model = model
+
+        self._signal_control = signal_control
+        self._signal_control.is_control_updated.connect(self._callback_signal_control)
+
+        self.layout = self._set_layout()
+
+    @Slot()
+    def _callback_signal_control(self, is_control_updated):
+        """Callback of the control signal.
+
+        Parameters
+        ----------
+        is_control_updated : `bool`
+            Control is updated or not.
+        """
+        raise NotImplementedError("Child class needs to realize this function.")
+
+    def _set_layout(self):
+        """Set the layout.
+
+        Returns
+        -------
+        layout : `PySide2.QtWidgets.QVBoxLayout`
+            Layout.
+        """
+        raise NotImplementedError("Child class needs to realize this function.")
